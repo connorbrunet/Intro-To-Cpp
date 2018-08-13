8fd82ec25e469231ec4a7e9b5c9fe5e7a0956353
Connor Brunet <connorbrunet@gmail.com>
2018-08-13 16:33:42 -0500

    Add project files.

---
+++ Intro to C++/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Connor Brunet\n";
+	std::cout << "Apple\n";
+	std::cout << "Orange\n";
+	std::cout << "Grape\n";
+	std::cout << "1.Firework\n";
+	std::cout << "2.Pour Some Sugar on me\n";
+	std::cout << "3.Passionfruit\n";
+	std::cout << "4.Open Your Eyes\n";
+	std::cout << "5.Endless\n";
+	std::cout << "6.Some Kind of Magic\n";
+	std::cout << "7.Praise the Lord (Da Shine)\n"; 
+	std::cout << "8.Remember The Name\n";
+	std::cout << "9.Turn It Up (Higher)\n";
+	std::cout << "10.Speed of Link\n"; 
+
+	std::cout << std::endl;
+
+	std::cout << "0\n";
+	std::cout << "1\n";
+	std::cout << "2\n";
+	std::cout << "3\n";
+	std::cout << "4\n";
+	std::cout << "5\n";
+
+	std::cout << std::endl;
+
+	std::cout << "5\n4\n3\n2\n1\n0\n";
+
+	system("pause");
+}
